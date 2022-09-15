da856b26c5f66c359f3074ab07c973cea551f197
Deepak Kumar <cu.18bcs2295@gmail.com>
2022-09-15 17:55:34 +0530

    register all entities in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -6,14 +7,15 @@ import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
+	ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'password',
-      database: 'realworld_nestjs',
-      entities: [],
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT) || 3306,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [ __dirname + '/**/*.entity{.ts,.js}' ],
       synchronize: true,
     }),
     UserModule,
