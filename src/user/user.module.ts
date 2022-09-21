6b70b3eee7406c7b3173efc763560218f9461e63
Deepak Kumar <cu.18bcs2295@gmail.com>
2022-09-21 15:21:55 +0530

    add auth middleware

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,14 +1,29 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+} from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { API_BASE_PREFIX } from 'src/utils/constants';
+import { AuthMiddleware } from './auth.middleware';
 import { UserController } from './user.controller';
 import { UserEntity } from './user.entity';
 import { UserService } from './user.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([
-	UserEntity
-  ])],
+  imports: [TypeOrmModule.forFeature([UserEntity])],
   controllers: [UserController],
-  providers: [UserService]
+  providers: [UserService],
+  exports: [UserService],
 })
-export class UserModule {}
+export class UserModule implements NestModule {
+  public configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(AuthMiddleware)
+      .exclude(
+		`${API_BASE_PREFIX}/user/login`,
+		`${API_BASE_PREFIX}/user/register`,
+      )
+      .forRoutes(UserController);
+  }
+}
